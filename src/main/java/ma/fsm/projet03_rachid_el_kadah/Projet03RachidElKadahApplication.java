5af1d4d8772d7daf7e8e4e32680c1af3e5b7ba24
R-ELKADAH <rachidelkadah@email.com>
2024-04-17 13:28:32 +0100

    H2 database

--- src/main/java/ma/fsm/projet03_rachid_el_kadah/Projet03RachidElKadahApplication.java
+++ src/main/java/ma/fsm/projet03_rachid_el_kadah/Projet03RachidElKadahApplication.java
@@ -1,13 +1,43 @@
 package ma.fsm.projet03_rachid_el_kadah;
 
+import ma.fsm.projet03_rachid_el_kadah.entities.Product;
+import ma.fsm.projet03_rachid_el_kadah.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projet03RachidElKadahApplication {
+import java.util.List;
 
+@SpringBootApplication
+public class Projet03RachidElKadahApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository productRepository;
 	public static void main(String[] args) {
+
 		SpringApplication.run(Projet03RachidElKadahApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		productRepository.save(new Product(null,"Computer",4300,3));
+		productRepository.save(new Product(null,"Printer",1200,4));
+		productRepository.save(new Product(null,"Smart phone",3200,32));
+		List<Product> products = productRepository.findAll();
+		products.forEach(p->{
+            System.out.println(p.toString());
+		});
+		Product product = productRepository.findById(Long.valueOf(1)).get();
+		System.out.println("**********");
+		System.out.println(product.getId());
+		System.out.println(product.getName());
+		System.out.println(product.getQuantity());
+		System.out.println("**********");
+
+
+
+
+	}
 }
--- src/main/java/ma/fsm/projet03_rachid_el_kadah/repository/ProductRepository.java
+++ src/main/java/ma/fsm/projet03_rachid_el_kadah/repository/ProductRepository.java
@@ -1,2 +1,7 @@
-package ma.fsm.projet03_rachid_el_kadah.repository;public interface ProductRepository {
+package ma.fsm.projet03_rachid_el_kadah.repository;
+
+import ma.fsm.projet03_rachid_el_kadah.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
